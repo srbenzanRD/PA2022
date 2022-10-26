b5ceaee78680f1d9d872d51f83c590c0573f910f
Felix Benzan <felixbenz07@gmail.com>
2022-10-26 19:47:48 -0400

    Crear Solicitud

--- Data/Models/Solicitud.cs
+++ Data/Models/Solicitud.cs
@@ -4,4 +4,15 @@ public class Solicitud{
     public int Id { get; set; }
     public DateTime Fecha { get; set; }
     public virtual ICollection<SolicitudSustentante>? Sustentantes {get; set;}
+
+    public static Solicitud Crear(List<Persona> personas){
+        var sustentantes = new List<SolicitudSustentante>();
+        personas.ForEach(persona=>{
+            sustentantes.Add(new SolicitudSustentante(){ personaId = persona.Id, persona = persona });
+        });
+        return new Solicitud(){
+                Fecha = DateTime.Now,
+                Sustentantes = sustentantes,
+        };
+    }
 }
